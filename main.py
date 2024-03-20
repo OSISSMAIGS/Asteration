efb84bfeec658f6c9ebf935b3b3065774597f165
TimothyDY <timothydhammattayuwana@students.ignatiusglobal.sch.id>
2024-03-20 13:10:54 +0700

    Qaliftar

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def asteration():
 def asteration_events():
     return render_template("asteration/events.html")
 
+@app.route("/Asteration/Events/Qaliftar")
+def e_qaliftar():
+    return render_template("asteration/e_qaliftar.html")
+
 @app.route("/Asteration/Events/Reveera")
 def e_reveera():
     return render_template("asteration/e_reveera.html")

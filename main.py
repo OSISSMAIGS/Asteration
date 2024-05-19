4521f30ddd0c0d3f0455fc6fbd2607e6c54d69c9
TimothyDY <timothydhammattayuwana@students.ignatiusglobal.sch.id>
2024-05-19 13:39:48 +0700

    first commit

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def asteration():
 def asteration_events():
     return render_template("asteration/events.html")
 
+@app.route("/Asteration/Events/Etourdissante")
+def e_etourdissante():
+    return render_template("asteration/e_etourdissante.html")
+
 @app.route("/Asteration/Events/Qaliftar")
 def e_qaliftar():
     return render_template("asteration/e_qaliftar.html")
@@ -24,7 +28,7 @@ def e_reveera():
 def e_lunerast():
     return render_template("asteration/e_lunerast.html")
 
-@app.route("/Asteration/Events/Litiorée")
+@app.route("/Asteration/Events/Litioree")
 def e_litioree():
     return render_template("asteration/e_litioree.html")
 
